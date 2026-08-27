502d38ae3e41c1e916f12559a90c2a85187c8cef
Mitchell <mgerb42@gmail.com>
2026-08-27 14:14:16 -0500

    feat: link Pipewire for audio on Linux (static and dynamic options)

--- example/src/main.zig
+++ example/src/main.zig
@@ -1,6 +1,15 @@
 const std = @import("std");
+const builtin = @import("builtin");
 const UI = @import("./ui.zig").UI;
 const Vulkan = @import("./vulkan.zig").Vulkan;
+
+const pipewire = if (builtin.os.tag == .linux) @import("pipewire") else struct {};
+
+// Pipewire is statically linked, but we aren't actually using it in here.
+// We must stub it in here so that it doesn't get optimized away.
+comptime {
+    _ = pipewire;
+}
 
 pub fn main(init: std.process.Init) !void {
     var gpa = std.heap.DebugAllocator(.{}){};
